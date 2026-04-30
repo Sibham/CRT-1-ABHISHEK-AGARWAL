001363ab0c98b134403d88bec17a8c9d258f09fa
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-04-30 18:50:03 +0530

    Add Swap2Variables.cpp for swapping two integers

---
+++ DAY-1/Swap2Variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int a , b; 
+    
+    cin>>a>>b;
+    //Swapping Logic
+    int temp = a;
+    a  = b;
+    b = temp;
+    
+   cout<<a<<"\n" <<b <<endl;
+   return 0;
+}
