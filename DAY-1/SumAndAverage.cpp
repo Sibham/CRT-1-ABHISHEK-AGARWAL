208c9fa0c49423ce6aaac997274c64e57cf28fbd
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-04-30 18:52:17 +0530

    Add Sum and Average calculation program

---
+++ DAY-1/SumAndAverage.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+   int a,b,c;
+   cin>>a>>b>>c;
+   
+   int total;
+   total = a + b+ c;
+   
+   cout<<"Sum of numbers :"<<total<<endl;
+   
+   float avg;
+   
+   avg = total/3.0;
+   
+   cout<<"avg of numbers :"<<avg<<endl;
+   return 0;
+}
