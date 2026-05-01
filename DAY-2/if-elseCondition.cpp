a02e36786f4b76c72b058c5e2511f8dc47679a47
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-05-01 09:27:44 +0530

    Create if-elseCondition.cpp

---
+++ DAY-2/if-elseCondition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+    cin>>age;
+    
+    //logic if condition
+    if(age>=18){
+        cout<<"Eligible for Code";
+    }
+    else{
+        //logic 
+        cout<<"Not eligible for Code";
+    }
+    return 0;
+}
