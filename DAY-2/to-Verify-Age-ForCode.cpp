1ba2295e22196d1d87b795c857cfc3c6c83b3c58
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-05-01 09:24:28 +0530

    Create to-Verify-Age-ForCode.cpp

---
+++ DAY-2/to-Verify-Age-ForCode.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+    cin>>age;
+    
+    //logic
+    if(age>=18){
+        cout<<"Eligible for Code";
+    }
+    return 0;
+}
