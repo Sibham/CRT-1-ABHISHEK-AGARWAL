47cbe384086d15ab9735896b6f7634b8e34aa284
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-05-01 10:57:41 +0530

    Create breakStatement.cpp

---
+++ DAY-2/breakStatement.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    int sum = 0;
+
+    while(true){
+        cin>>n;
+        
+        if(n<0){
+            break;
+        }
+        sum = sum + n;
+    }
+    cout<<sum;
+	return 0;
+}
