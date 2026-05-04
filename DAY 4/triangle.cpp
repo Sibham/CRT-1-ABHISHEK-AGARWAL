313290d7af2906cd0506e230b60123bbeefd7aca
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-05-04 11:52:47 +0530

    Create triangle.cpp

---
+++ DAY 4/triangle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    
+    
+    //no. of rows
+  for( int i = 1 ; i<=n ; i++){
+      
+      for(int j = n-i ; j>=1; j--){
+       {
+        cout<<" ";
+         }
+      }
+       for( int k = 1 ; k<= i ; k++){
+       cout<<" *";
+       }
+      cout<<"\n";
+  }
+    
+}
