7a292fbe01bf667d891da8c9681ac0e31f3ac6dc
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-05-04 09:31:46 +0530

    Update pattern1.cpp

--- DAY 4/pattern1.cpp
+++ DAY 4/pattern1.cpp
@@ -4,7 +4,9 @@
 using namespace std;
 int main()
 {
+  // number of times to print the pattern
   for( int i = 1 ; i<=3 ; i++){
+    // this defimes what to print in the psttern
       for(int j = 1 ; j<= 3 ; j++){
           cout<<j;
       }
