4ce96418239355294396882fd367ac6cfc8455a1
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-05-05 10:31:21 +0530

    Create argu-Noreturn.cpp

---
+++ DAY 5 FUNCTIONS /argu-Noreturn.cpp
@@ -0,0 +1,14 @@
+
+//Problem 4
+//4.2 - Argument and No return Type
+
+#include <iostream>
+using namespace std;
+
+void sendText(string message) {
+	cout<<message;
+}
+int main() {
+	sendText("BJP wons");
+	return 0;
+}
