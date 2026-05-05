77227d23d125cc567bede3ab9518b0affb9b14f5
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-05-05 10:33:33 +0530

    Create NOargu-return.cpp

---
+++ DAY 5 FUNCTIONS /NOargu-return.cpp
@@ -0,0 +1,14 @@
+
+//Problem 4
+//4.3 - Argument ,return Type
+
+#include <iostream>
+using namespace std;
+
+int ticketNumber() {
+	return 101;
+}
+int main() {
+	cout<<ticketNumber();
+	return 0;
+}
