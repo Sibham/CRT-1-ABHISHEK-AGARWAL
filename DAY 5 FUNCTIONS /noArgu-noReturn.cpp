cda05a0e7fb3deaba3f8277c09cde7d8dcef02e7
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-05-05 10:26:39 +0530

    Create noArgu-noReturn.cpp

---
+++ DAY 5 FUNCTIONS /noArgu-noReturn.cpp
@@ -0,0 +1,13 @@
+//Problem 4
+//4.1 - No Argument and No return Type
+
+#include <iostream>
+using namespace std;
+
+void doorBell() {
+	cout<<"Ting Tong";
+}
+int main() {
+	doorBell();
+	return 0;
+}
