de3fca975b768c4966de7d6cac3b47b3dd93e146
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-05-07 11:05:47 +0530

    Update defaultConstructor.cpp

--- DAY 7 /defaultConstructor.cpp
+++ DAY 7 /defaultConstructor.cpp
@@ -1,39 +1,3 @@
-// //PROBLEM 3
-//Create a Vehicle class:
-//engineNumber → private
-//brand → protected
-//model → public
-//Child class Car should display brand and model
-
-
- /*#include <iostream>
- using namespace std;
-
- class Parent{
-  protected:   
-     string brand = "BMW";
- };
-
- class child: public Parent{
-     
-private:
-int engineNumber = 2023234;
-
-public:
-    void car(){
-        string model = "M4";
-       cout<<"THE MODEL OF CAR :"<<model<<"\n"<<"THE BRAND OF CAR :" <<brand;
-    }
-};
-
-int main()
-{
-  child obj1;
-  obj1.car();
-
-
-     return 0;
-}*/
 
 //2.1 = DEFAULT CONSTRUCTOR
 #include <iostream>
