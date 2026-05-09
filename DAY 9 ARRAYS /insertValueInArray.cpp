519bc9df129667df0addad9b870be8acee8ba7f8
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-05-09 11:30:42 +0530

    Create insertValueInArray.cpp

---
+++ DAY 9 ARRAYS /insertValueInArray.cpp
@@ -0,0 +1,23 @@
+//Insert on element in an array
+#include<iostream>
+using namespace std;
+void print(int arr[], int size){
+    for(int i = 0 ; i< size ; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<"\n";
+}
+void InsertElement(int arr[],int size , int index , int value){
+    for(int i = size ; i>index ; i--){
+        arr[i] = arr[i - 1];
+    }
+    arr[index] = value;
+} 
+
+
+int main(){
+    int arr[] = {10,20,30,40,50};
+    InsertElement(arr , 5 , 2,25);
+    print(arr , 6);
+    
+}
