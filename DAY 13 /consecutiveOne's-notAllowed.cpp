28e15136a2864a4c4a2a12a7f1a7708b38063fa2
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-05-13 08:52:45 +0530

    Create  consecutiveOne's-notAllowed.cpp

---
+++ DAY 13 /consecutiveOne's-notAllowed.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int consecutiveOne(int n){
+    //base case
+    if(n==0)
+    return 1;
+    
+    if(n==1){
+        return 3;
+    }
+    
+    if(n==2){
+        return 3;
+    }
+    
+    return consecutiveOne(n-1) + consecutiveOne(n-2);
+}
+
+int main(){
+    cout<<consecutiveOne(3);
+}
